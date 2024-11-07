58795db70440e06d412bf8c7caa29621c711b2ea
Nastya <karmazina133@gmail.com>
2024-11-07 21:54:34 +0200

    Звіт до лаб 2

---
+++ Lab_2/DownloadManager/DownloadManager/Models/Download.cs
@@ -0,0 +1,22 @@
+﻿using DownloadManager.Enums;
+
+namespace DownloadManager.Models
+{
+    public class Download
+    {
+        public int ID { get; set; }
+        public DownloadStatus Status { get; set; }
+        public string FilePath { get; set; }
+        public int SpeedLimit { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public long DownloadedSize { get; set; }
+        public long TotalSize { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Download()
+        {
+            CreatedAt = DateTime.UtcNow;
+        }
+    }
+}
---
+++ Lab_2/DownloadManager/DownloadManager/Models/DownloadHistory.cs
@@ -0,0 +1,15 @@
+﻿using DownloadManager.Enums;
+
+
+namespace DownloadManager.Models
+{
+    public class DownloadHistory
+    {
+        public int ID { get; set; }
+        public int DownloadID { get; set; }
+        public DateTime Date { get; set; }
+        public DownloadStatus Status { get; set; }
+
+        public Download Download { get; set; }
+    }
+}
